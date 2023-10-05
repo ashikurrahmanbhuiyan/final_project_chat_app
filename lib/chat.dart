e79a6b570553db7c8ca422fa55967d5469acb227
ashik <s2010376107@ru.ac.bd>
2023-10-05 18:43:39 +0600

    nothing, working fine

--- lib/chat.dart
+++ lib/chat.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-
-import 'package:final_project_chat_app/widgets/user_image_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -31,21 +28,10 @@ class ChatScreen extends StatelessWidget {
           ) 
         ],
       ),
-      body: Column(
+      body: const Column(
         children: [
-          const Center(
+           Center(
             child: Text('Chat Screen'),
-          ),
-          UserImagePicker(
-            onPickImage: (userImage) {
-              //show image on screen
-                CircleAvatar(
-                  radius: 40,
-                  backgroundColor: Colors.grey,
-                  // ignore: unnecessary_null_comparison
-                  foregroundImage: userImage!=null? FileImage(userImage):null,
-                );
-            }
           ),
         ],
       ),
